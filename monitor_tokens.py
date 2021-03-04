e5810bb99ec6ccd36c08ac96ef63b55aaa65023e
tarou159f <r201603849je@jindai.jp>
2021-03-04 23:23:34 +0900

    Add files via upload
    add monitor_tokens.py

--- monitor_tokens.py
+++ monitor_tokens.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import urllib.request, urllib.error
+import re
 from search_tokens import search_address_from_csv, write_token_data, token_file as found_tokens_file
 monitor_tokens_file = "./monitor_tokens.csv"
 
@@ -9,16 +10,27 @@ def check_url(url):
     try:
         f = urllib.request.urlopen(url)
         f.close
+        print(url)
         return True
     except:
         return False
 
 
 def main():
+    schemes = ["http://", "https://"]
+    domains = [".com", ".org", ".app", ".io", ".finance", ".farm", ".net"]
+    filter_list = ["Token", "token", "TOKEN", "Swap", "swap", "SWAP", "Finance", "finance", "FINANCE", "t.me/", ".", "BSC"]
     if os.path.exists(monitor_tokens_file):
-        rdf1 = pd.read_csv(found_tokens_file, usecols=[0, 6])
-        rdf1["Website"] = None
         rdf2 = pd.read_csv(monitor_tokens_file)
+        for token in rdf2["TokenTracker"]:
+            phrase = token.split(" ")[:-1]
+            phrase = [w for w in phrase if w not in filter_list]
+            for f in filter_list:
+                phrase = [w.replace(f, "") for w in phrase]
+            for scheme in schemes:
+                for domain in domains:
+                    url = scheme + ','.join(phrase) + domain
+                    check_url(url)
 
     else:
         rdf = pd.read_csv(found_tokens_file, usecols=[0, 6])
